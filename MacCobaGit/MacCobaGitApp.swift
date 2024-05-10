ce39290712aee945ec73ec84322e0747c9cb8345
Marshanda Gwie <marshandasugiarto@gmail.com>
2024-05-10 08:31:53 +0700

    bikin MacOS target

---
+++ MacCobaGit/MacCobaGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacCobaGitApp.swift
+//  MacCobaGit
+//
+//  Created by Marshanda Gwie on 10/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MacCobaGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
