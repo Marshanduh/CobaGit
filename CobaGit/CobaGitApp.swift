bf4925fb5d2f2f78411428e231e937109f96706b
Marshanda Gwie <marshandasugiarto@gmail.com>
2024-05-10 08:29:39 +0700

    Initial Commit

---
+++ CobaGit/CobaGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CobaGitApp.swift
+//  CobaGit
+//
+//  Created by Marshanda Gwie on 10/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CobaGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WatchCobaGit Watch App/WatchCobaGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchCobaGitApp.swift
+//  WatchCobaGit Watch App
+//
+//  Created by Marshanda Gwie on 10/05/24.
+//
+
+import SwiftUI
+
+@main
+struct WatchCobaGit_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
